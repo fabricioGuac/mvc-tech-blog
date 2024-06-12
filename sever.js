6c64964f34c85b17c5f86c3eae7c524c5e2799a5
Fabricio Guacuto <guacutofabricio@gmail.com>
2024-06-12 16:06:19 -0700

    enhance server setup in server.js. require packages, add middleware, and initialize server with database connection

--- sever.js
+++ sever.js
@@ -1,1 +1,54 @@
+// Loads enviromental variables
 require('dotenv').config();
+
+// Imports the required packages
+const path = require('path')
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+// Creates an express application
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Creates an instance of Handlebars engine
+const hbs = exphbs.create();
+
+// Requires the sequelize connection
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// Configures the session
+const sess = {
+    secret: process.env.SECRET,
+    cookie: {
+      maxAge:  3600000,
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+};
+
+// Sets handlebars as the view engine 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Sets up session middleware
+app.use(session(sess));
+
+// Sets up middleware for parsing url and json encoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serves the static files in the public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Sync Sequelize models with the database and start the server
+sequelize.sync().then(() => {
+    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+});
