2c5f0af9eab94de090c57e118fe9d98ee448a192
Fabricio Guacuto <guacutofabricio@gmail.com>
2024-07-24 14:24:04 -0700

    moves the route to render the chats from the message route to home routes

--- controller/api/messageRoutes.js
+++ controller/api/messageRoutes.js
@@ -23,54 +23,5 @@ router.post('/',auth, async (req, res) => {
 });
 
 
-// Function to get the chat between two users
-router.get('/:id',  auth, async (req, res) =>{
-
-    //Deconstructs the id of the target user
-    const {id} = req.params;
-
-    // Gets the id of the current logged in user
-    const currentUserId = req.session.user_id;
-
-    try {
-        // Retrieves all messages where the sender and receiver are either the target user and the current user, or vice versa
-        const messages = await Message.findAll({
-            where:{
-                [Op.or]: [
-                    {sender_id: id, receiver_id: currentUserId},
-                    {sender_id: currentUserId , receiver_id: id },
-                ],
-            },
-            // Orders them in ascending order
-            order: [['date', 'ASC']],
-                // Joins with the user table on the username for the sender and receiver
-                include: [
-                    {
-                        model: User,
-                        as: 'Sender',
-                        attributes: ['username'],
-                    },
-                    {
-                        model: User,
-                        as: 'Receiver',
-                        attributes: ['username'],
-                    },
-                ],
-        });
-
-        const chat = messages.map(message => message.get({ plain: true }));
-
-
-        res.status(200).render('chat',{
-            chat,
-            logged_in: req.session.logged_in,
-            currentUserId: currentUserId
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
-})
-
-
 // Exports the routes
 module.exports = router;
